8bace3f1f85fbfc85a9d7657f157c9e9a220e853
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-08-07 22:58:25 +0300

    added semicolons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var TIMEOUT_IN_SECS = 3 * 60
-var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>'
+var TIMEOUT_IN_SECS = 3 * 60;
+var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>';
 var QUOTES = ['Любишь ты жизнь? Тогда не теряй времени; ибо время — ткань, из которой состоит жизнь.',
 'Каждое потерянное мгновение — потерянное дело, потерянная польза.',
 'Что ты делаешь сейчас? Это приближает тебя к достижению целей?',
@@ -10,32 +10,32 @@ var QUOTES = ['Любишь ты жизнь? Тогда не теряй врем
 'Хорошее употребление времени делает время еще более драгоценным.'];
 
 // adds HTML tag to current page
-var timerContainer = document.createElement('div')
-timerContainer.setAttribute("style", "height: 100px;")
-var bodyTag = document.body
-bodyTag.insertBefore(timerContainer, bodyTag.firstChild)
-timerContainer.innerHTML = TEMPLATE
+var timerContainer = document.createElement('div');
+timerContainer.setAttribute("style", "height: 100px;");
+var bodyTag = document.body;
+bodyTag.insertBefore(timerContainer, bodyTag.firstChild);
+timerContainer.innerHTML = TEMPLATE;
 
 function getTimestampInSecs(){
-  var timestampInMilliseconds = new Date().getTime()
-  return Math.round(timestampInMilliseconds/1000)
+  var timestampInMilliseconds = new Date().getTime();
+  return Math.round(timestampInMilliseconds/1000);
 }
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
 }
 
-var timestampOnStart = getTimestampInSecs()
+var timestampOnStart = getTimestampInSecs();
 
 function displayTimer(){
-  var currentTimestamp = getTimestampInSecs()
+  var currentTimestamp = getTimestampInSecs();
   var secsGone = currentTimestamp - timestampOnStart
-  var secsLeft = Math.max(TIMEOUT_IN_SECS - secsGone, 0)
+  var secsLeft = Math.max(TIMEOUT_IN_SECS - secsGone, 0);
 
   var minutes = Math.floor(secsLeft / 60);
   var seconds = secsLeft - minutes * 60;
 
-  document.getElementById('timer-minutes').innerHTML = padZero(minutes)
-  document.getElementById('timer-seconds').innerHTML = padZero(seconds)
+  document.getElementById('timer-minutes').innerHTML = padZero(minutes);
+  document.getElementById('timer-seconds').innerHTML = padZero(seconds);
 }
-setInterval(displayTimer, 300)
+setInterval(displayTimer, 300);
