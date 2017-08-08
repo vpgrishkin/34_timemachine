50d46f36b81799d9b0b545fc29a78c1d9e33e586
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-08-08 10:10:05 +0300

    fix some misprints and the code inside a function (not glob var)

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+function() {
 var TIMEOUT_IN_SECS = 3 * 60;
 var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>';
 var QUOTES = ['Любишь ты жизнь? Тогда не теряй времени; ибо время — ткань, из которой состоит жизнь.',
 'Каждое потерянное мгновение — потерянное дело, потерянная польза.',
 'Что ты делаешь сейчас? Это приближает тебя к достижению целей?',
 'Время — деньги.',
-'Если хочешь иметь досуг, не теряй времени даром.'
-'Мудрое распределение времени есть основа для деятельности.'
-'Время — это простор для развития способностей...'
+'Если хочешь иметь досуг, не теряй времени даром.',
+'Мудрое распределение времени есть основа для деятельности.',
+'Время — это простор для развития способностей...',
 'Хорошее употребление времени делает время еще более драгоценным.'];
 
 // adds HTML tag to current page
@@ -29,7 +30,7 @@ var timestampOnStart = getTimestampInSecs();
 
 function displayTimer(){
   var currentTimestamp = getTimestampInSecs();
-  var secsGone = currentTimestamp - timestampOnStart
+  var secsGone = currentTimestamp - timestampOnStart;
   var secsLeft = Math.max(TIMEOUT_IN_SECS - secsGone, 0);
 
   var minutes = Math.floor(secsLeft / 60);
@@ -39,3 +40,4 @@ function displayTimer(){
   document.getElementById('timer-seconds').innerHTML = padZero(seconds);
 }
 setInterval(displayTimer, 300);
+}();
